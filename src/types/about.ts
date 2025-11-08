04fa99254d6af0e113a7c175c2505f7dc9f99439
Junwon <26790365+junuuon@users.noreply.github.com>
2025-11-08 12:43:08 +0900

    Add data interface

---
+++ src/types/about.ts
@@ -0,0 +1,52 @@
+export interface IntroductionProps {
+  briefing: string[];
+  githubLink: string;
+  linkedinLink: string;
+  mailLink: string;
+  title: string;
+}
+
+interface ProjectProps {
+  project: {
+    dateFrom: string;
+    dateTo?: string;
+    description: string;
+    detail: string[];
+    detailLink?: string;
+    githubLink?: string;
+    productLink?: string;
+    skill: string;
+    title: string;
+  }[];
+}
+export interface WorkExperienceProps extends ProjectProps {
+  additional?: {
+    label: string;
+    link: string;
+  };
+  companyName: string;
+  dateFrom: string;
+  dateTo?: string;
+  role: string;
+}
+
+export interface OtherExperienceProps extends ProjectProps {
+  additional?: {
+    label: string;
+    link: string;
+  };
+  companyName?: string;
+  dateFrom?: string;
+  dateTo?: string;
+  role?: string;
+}
+
+export type CertificateProps = {
+  label: string;
+  link: string;
+};
+
+export type SkillProps = {
+  list: string[];
+  title: string;
+};
---
+++ src/types/post.ts
@@ -0,0 +1,8 @@
+export type PostType = {
+  date?: string;
+  description?: string;
+  image?: string;
+  originalLink?: string;
+  slug: string;
+  title: string;
+};
